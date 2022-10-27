061fffa704e929a6e321558ff2681de2702dd723
许正韬 <1753059613@qq.com>
2022-10-27 12:10:24 +0800

    add CourseContent

--- src/views/HomeView/CourseContent.vue
+++ src/views/HomeView/CourseContent.vue
@@ -1,11 +1,33 @@
 <template>
   <v-container fluid style="background-color: red">
-    <v-card> 123 </v-card>
+	<v-row>
+		<!-- Course WorkPlace -->
+		<v-col cols="12" sm="8">
+			<v-card> 
+				<v-tabs v-model="tabs" centered>
+					<v-tab>作业</v-tab>
+					<v-tab>考试</v-tab>
+					<v-tab>公告</v-tab>
+				</v-tabs>
+			</v-card>			
+		</v-col>
+		<!-- Course Info -->
+		<v-col cols="12" sm="4">
+			<v-card>123</v-card>
+		</v-col>
+	</v-row>
+
   </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+	data() {
+		return {
+			tabs:3,
+		}
+	}
+};
 </script>
 
 <style></style>
