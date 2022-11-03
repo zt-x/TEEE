17f6bc20e284d3ac2aa16797b4213c38e4b5f761
许正韬 <1753059613@qq.com>
2022-11-04 00:50:17 +0800

    submitWork

---
+++ src/components/CourseContentChildren/work/submitWork.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card class="my-1 mx-auto" style="width: 95%">
+    <v-container>
+      <v-row>
+        <v-col cols="2">{{ INFO.name }}</v-col>
+        <v-col cols="2">{{ INFO.username }}</v-col>
+        <v-col cols="8">
+          <v-spacer></v-spacer>
+          <v-chip color="warning" small>{{ INFO.score }}</v-chip>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["INFO"],
+};
+</script>
+
+<style></style>
--- src/views/HomeView/Course/AWorkContent.vue
+++ src/views/HomeView/Course/AWorkContent.vue
@@ -1,54 +1,57 @@
 <template>
   <v-container fluid>
-	<v-row>
+    <v-row>
       <!-- Submitted WorkPlace -->
       <v-col cols="12" sm="8">
-		<v-card>
-			<v-tabs v-model="tab" background-color="transparent" color="basil">
-				<v-tab>
-					<v-chip color="warning" small>111</v-chip>
-					<span class="pl-2">未批改</span>
-				</v-tab>	
-				<v-tab>
-					<v-chip small color="success">12</v-chip>
-					<span class="pl-2">已批改</span>
-				</v-tab>
-			</v-tabs>
-			<v-tabs-items v-model="tab">
-				<v-tab-item>
-					<v-card color="basil" flat>
-
-					</v-card>
-				</v-tab-item>
-				<v-tab-item>
-					<v-card color="basil" flat>
-
-					</v-card>
-				</v-tab-item>
-			</v-tabs-items>
+        <v-card>
+          <v-tabs v-model="tab" background-color="transparent" color="basil">
+            <v-tab>
+              <v-chip color="warning" small>111</v-chip>
+              <span class="pl-2">未批改</span>
+            </v-tab>
+            <v-tab>
+              <v-chip small color="success">12</v-chip>
+              <span class="pl-2">已批改</span>
+            </v-tab>
+          </v-tabs>
+          <v-tabs-items v-model="tab">
+            <v-tab-item>
+              <v-card color="basil" flat class="pt-3">
+                <SubmitWork v-for="(info, index) in infos" :key="index" :INFO="info" />
+              </v-card>
+            </v-tab-item>
+            <v-tab-item>
+              <v-card color="basil" flat> </v-card>
+            </v-tab-item>
+          </v-tabs-items>
         </v-card>
-	  </v-col>
-	  <!-- Statistic -->
+      </v-col>
+      <!-- Statistic -->
       <v-col cols="12" sm="4">
-		<v-card>
-			<v-card-title>111</v-card-title>
-		</v-card>
-	  </v-col>
-	</v-row>	
+        <v-card>
+          <v-card-title>111</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import SubmitWork from "@/components/CourseContentChildren/work/submitWork.vue";
 export default {
-	data() {
-		return {
-			tab: null,
-			items: ['未批改', '已批改'],
-		}
-	}
-}
+  components: { SubmitWork },
+  data() {
+    return {
+      tab: null,
+      items: ["未批改", "已批改"],
+      infos: [
+        { name: "xzt", username: "2020", score: 99 },
+        { name: "xzt", username: "2020", score: 99 },
+        { name: "xzt", username: "2020", score: 99 },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
