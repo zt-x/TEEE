38aacd565843bb69bbfce5e066198fb1047ab4da
许正韬 <1753059613@qq.com>
2022-11-09 23:58:40 +0800

    fix submitWork

--- src/components/CourseContentChildren/work/submitWork.vue
+++ src/components/CourseContentChildren/work/submitWork.vue
@@ -2,11 +2,11 @@
   <v-card class="my-1 mx-auto" style="width: 95%">
     <v-container>
       <v-row>
-        <v-col cols="2">{{ INFO.name }}</v-col>
-        <v-col cols="2">{{ INFO.username }}</v-col>
+        <v-col cols="2">{{ SUBMIT.username }}</v-col>
+        <v-col cols="2">{{ SUBMIT.uid }}</v-col>
         <v-col cols="8">
           <v-spacer></v-spacer>
-          <v-chip color="warning" small>{{ INFO.score }}</v-chip>
+          <v-chip color="warning" small>{{ SUBMIT.score }}</v-chip>
         </v-col>
       </v-row>
     </v-container>
--- src/views/HomeView/Course/AWorkContent.vue
+++ src/views/HomeView/Course/AWorkContent.vue
@@ -1,3 +1,4 @@
+·
 <template>
   <v-container fluid>
     <v-row>
@@ -6,25 +7,35 @@
         <v-card>
           <v-tabs v-model="tab" background-color="transparent" color="basil">
             <v-tab>
-              <v-chip color="warning" small>111</v-chip>
+              <v-chip color="warning" small>{{ numOfUnFihish }}</v-chip>
               <span class="pl-2">未批改</span>
             </v-tab>
             <v-tab>
-              <v-chip small color="success">12</v-chip>
+              <v-chip small color="success">{{ numOfFinish }}</v-chip>
               <span class="pl-2">已批改</span>
             </v-tab>
           </v-tabs>
           <v-tabs-items v-model="tab">
             <v-tab-item>
               <v-card color="basil" flat class="pt-3">
-                <SubmitWork v-for="(submit, index) in submits" :key="index" :SUBMIT="submit" />
+                <SubmitWork
+                  v-for="(submit, index) in submit_unfinish"
+                  :key="index"
+                  :SUBMIT="submit"
+                />
               </v-card>
             </v-tab-item>
             <v-tab-item>
-              <v-card color="basil" flat> </v-card>
+              <v-card color="basil" flat class="pt-3">
+                <SubmitWork
+                  v-for="(submit, index) in submit_finish"
+                  :key="index"
+                  :SUBMIT="submit"
+                />
+              </v-card>
             </v-tab-item>
           </v-tabs-items>
-		  <div style="height:10px"></div>
+          <div style="height: 10px"></div>
         </v-card>
       </v-col>
       <!-- Statistic -->
@@ -43,8 +54,23 @@ import axios from "axios";
 const _axios = axios.create();
 let token = window.localStorage.getItem("token");
 export default {
-	
   components: { SubmitWork },
+  computed: {
+    wid() {
+      if (this.$route.params.wid == null) {
+        //测试环境
+        return 29;
+      } else {
+        return this.$route.params.wid;
+      }
+    },
+    numOfFinish() {
+      return this.submit_finish.length;
+    },
+    numOfUnFihish() {
+      return this.submit_unfinish.length;
+    },
+  },
   data() {
     return {
       tab: null,
@@ -54,35 +80,73 @@ export default {
         { name: "xzt", username: "2020", score: 99 },
         { name: "xzt", username: "2020", score: 99 },
       ],
+      submit_finish: [],
+      submit_unfinish: [],
     };
-},
-	methods: {
-		async getAllSubmits() {
-			token = window.localStorage.getItem("token");
-			let _this = this;
-			// init axios
-			_axios.interceptors.request.use(function (config) {
-				config.headers = {
-				Authorization: token,
-				};
-				return config;
-			});
-			const form = new FormData();
-			form.append("wid", this.wid);
-			_axios
-				.post("/api/Work/getWork", form)
-				.then((res) => {
-				let questions = res.data.data;
-				_this.qs = eval(questions);
-				_this.myAnss.length = _this.qs.length;
-				})
-				.catch((err) => {});
-		},
-	},
-	mounted() {
+  },
+  methods: {
+    async getAllSubmits() {
+      token = window.localStorage.getItem("token");
+      let _this = this;
+      // init axios
+      _axios.interceptors.request.use(function (config) {
+        config.headers = {
+          Authorization: token,
+        };
+        return config;
+      });
+      const form = new FormData();
+      form.append("wid", this.wid);
+      _axios
+        .post("/api/submit/getAllSubmitByWorkId", form)
+        .then((res) => {
+          let submits = res.data.data;
+          _this.submits = eval(submits);
 
-	}
-   
+          //分离已完成批改和未完成批改的
+          _this.submit_finish = _this.submits.filter((item) => {
+            return item.finishReadOver == 1;
+          });
+          _this.submit_unfinish = _this.submits.filter((item) => {
+            return (
+              item.finishReadOver == 0 ||
+              item.finishReadOver == undefined ||
+              item.finishReadOver == null
+            );
+          });
+        })
+        .catch((err) => {});
+    },
+    async getStatistics() {
+      const form = new FormData();
+      form.append("wid", this.wid);
+      _axios
+        .post("/api/submit/getAllSubmitByWorkId", form)
+        .then((res) => {
+          let submits = res.data.data;
+          _this.submits = eval(submits);
+
+          //分离已完成批改和未完成批改的
+          _this.submit_finish = _this.submits.filter((item) => {
+            return item.finishReadOver == 1;
+          });
+          _this.submit_unfinish = _this.submits.filter((item) => {
+            return (
+              item.finishReadOver == 0 ||
+              item.finishReadOver == undefined ||
+              item.finishReadOver == null
+            );
+          });
+        })
+        .catch((err) => {});
+    },
+  },
+  mounted() {
+    let _this = this;
+    this.getAllSubmits().then(() => {
+      _this.getStatistics();
+    });
+  },
 };
 </script>
 
